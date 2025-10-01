34c459769b164b855aa21866379c39407badcb10
LUIZ GUILHERME GARCIA <luiz_g_garcia@estudante.sesisenai.org.br>
2025-10-01 17:01:21 -0300

    tratando sistema de reconexão

--- MQTT_basic/MQTT_basic.ino
+++ MQTT_basic/MQTT_basic.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const String SSID = "iPhone";
+const String PSWD = "iot_sul_123";
+void conectarWiFi()
 
+
+void setup() {
+  Serial.begin(115200);
+  conectarWiFi();  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("tentando reconectar: aguarde");
+    conectarWiFi();
+  }
+}
 
+void conectarWiFi(){
+  Serial.println("iniciando conexão com rede WiFi");
+  WiFi.begin(SSID, PSWD);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\n Conectado!");
 }
