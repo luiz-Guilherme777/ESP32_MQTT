383182f5375a441f91e42cb1218d8fc6806cfac0
LUIZ GUILHERME GARCIA <luiz_g_garcia@estudante.sesisenai.org.br>
2025-10-01 15:26:11 -0300

    implementada scan de redes WiFi com ESP32C6

--- wifi_teste/wifi_teste.ino
+++ wifi_teste/wifi_teste.ino
@@ -1,9 +1,23 @@
+#include <WiFi.h>
+
+
  void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("iniciando scan de redes Wi-Fi");
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int number = WiFi.scanNetworks();
+  delay(500);
+  if(number == -1){
+    Serial.println("ERRO!. Não deu bom");
+  }
+  else{
+    Serial.printf("Numero das redes encontradas : %d\n", number);
+    for(int net = 0; net < number; net++){
+      Serial.printf("%d - %s | %d db\n", net, WiFi. SSID(net), WiFi.RSSI(net));
+    }
+  }
 
 }
